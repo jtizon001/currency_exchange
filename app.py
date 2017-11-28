0a877114c2d2a9e5aa753aec8a581f4f98ebd650
Sam Snider <sam.snider301@gmail.com>
2017-11-27 21:08:10 -0500

    edited wrong thing now fixing it back

--- app.py
+++ app.py
@@ -1,51 +1,42 @@
+from flask import Flask, request, url_for, jsonify, json
+from flask_restful import reqparse, abort, Api, Resource
 
-$(document).ready(function () {
+import foreXchange
 
-alert("hello");
+app = Flask(__name__)
 
-//use ajax to fetch data from the python file "app.py"
+jsonExample = {"base":"USD","target":"CNY","amount":"100"}
 
+@app.route('/getData', methods=['POST'])
+def getData():
+  if request.headers['Content-Type'] == 'application/json':
+    forXargs=request.get_json()
+    base=forXargs['base'].upper()
+    print(base)
+    target=forXargs['target'].upper()
+    amount=int(forXargs['amount'])
+    x=0
+    if (base=="BTC" or target=="BTC"):
+      x=foreXchange.convertBit(base,target,amount)
+    else:
+      x=foreXchange.convertAmount(base,target,amount)
+    dicto={"rate":x}
+    response=jsonify(dicto)
+    return response
 
-  $( document.getElementById('button1') ).click(function() {
-    alert("you clicked");
-
-    var dataObject = $('form').serialize();
-    console.log(dataObject);
-
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:5000/getData",
-      // dataType: "JSON",
-      // data: {"base":"btc","target":"usd","amount":"100"},
-      data: {"d": dataObject},
-      error: function(response){
-        console.log("error");
-        alert("error");
-      },
-      success: function(response){
-        console.log(response);
-        alert(response);
-        // $( document.getElementById('resultDiv') ).html(response);
-      }
-
-    }).done(function( ) {
-      
-      console.log("done");
-
-    });
-
-    alert("2");
-
- 
-  });
- 
+@app.route('/index',methods=['GET','POST']) 
+def index():
+  x= open('index.html')
+  return x.read()
 
 
+if __name__ == '__main__':
+  app.run(debug=True)
 
 
-//   $.getJSON('http://example.com/your/webservice?param1=x&param2=y', 
-//     function(data, textStatus, jqXHR) {
-//         alert(data);
-//     }
-// )
-});
+# when testing use 
+# curl -H "Content-Type: application/json" -X POST -d '{"base":"btc","target":"usd","amount":"100"}' http://localhost:5000/getData
+
+
+# when testing use 
+# curl -H "Content-Type: application/json" -X POST -d '{"base":"btc","target":"usd","amount":"100"}' http://localhost:5000/index
