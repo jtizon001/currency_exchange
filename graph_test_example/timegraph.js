6749b58fa06e08b209196582b26a937365f9a3ba
qwang22 <33671464+qwang22@users.noreply.github.com>
2017-12-07 16:15:37 -0500

    updated with a few comments for clarity

--- graph_test_example/timegraph.js
+++ graph_test_example/timegraph.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
 
-	// 2 arrays with currency rate info of certain time frame
+	// 2 arrays with dummy currency rate info of certain time frame
+	// these 2 arrays should be returned by some database access call
 	var currency1 = [10, 20, 30, 40, 50]
 	var currency2 = [5, 20, 40, 55, 60]
 
+	//array of dates corresponding to above currency rates
 	var x = ['2016-01-01', '2016-01-02', '2016-01-03', '2016-01-04', '2016-01-05']
 
+	// Needed for proper format of graph creation
 	// Adds currency name to head of array
-	currency1.unshift('currency1');
-	currency2.unshift('currency2');
+	currency1.unshift('currency1');  //currency1 = ['currency1', 10, 20, 30,...]
+	currency2.unshift('currency2');  //currency2 = ['currency2', 5, 20, 40,...]
 
-	x.unshift('x');
+	x.unshift('x');  //x = ['x', '2016-01-01',....]
 
 	var chart = c3.generate({
 
