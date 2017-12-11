2abe948f9a5c3740685c041b3232cf7900c23751
Sam Snider <sam.snider301@gmail.com>
2017-12-10 21:37:46 -0500

    our recent attempts at fixing it

--- app.py
+++ app.py
@@ -9,7 +9,52 @@ jsonExample = {"base":"USD","target":"CNY","amount":"100"}
 
 @app.route('/getData', methods=['POST'])
 def getData():
+  if request.headers['Content-Type'] == 'application/json':from flask import Flask, request, url_for, jsonify, json
+from flask_restful import reqparse, abort, Api, Resource
+
+import foreXchange
+
+app = Flask(__name__)
+
+jsonExample = {"base":"USD","target":"CNY","amount":"100"}
+
+@app.route('/getData', methods=['POST'])
+def getData():
   if request.headers['Content-Type'] == 'application/json':
+    forXargs=request.get_json()
+    base=forXargs['base'].upper()
+    print(base)
+    target=forXargs['target'].upper()
+    amount=int(forXargs['amount'])
+    x=0
+    if (base=="BTC" or target=="BTC"):
+      x=foreXchange.convertBit(base,target,amount)
+    else:
+      x=foreXchange.convertAmount(base,target,amount)
+    dicto={"rate":x}
+    response=jsonify(dicto)
+    return response
+
+# @app.route('/',methods=['GET','POST']) 
+# def index():
+#   x= open('static/index.html')
+#   return x.read()
+
+@app.route('/') 
+def index():
+  return app.send_static_file("index.html");
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
+# when testing use 
+# curl -H "Content-Type: application/json" -X POST -d '{"base":"btc","target":"usd","amount":"100"}' http://localhost:5000/getData
+
+
+
+
     forXargs=request.get_json()
     base=forXargs['base'].upper()
     print(base)
