6dca4e7fc8af753a9af9157bbe12daa2e4a54ff5
Sam Snider <sam.snider301@gmail.com>
2017-12-10 19:42:12 -0500

    bug fix?

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
       $.ajax({
         method: "POST",
-        url: "192.241.142.13/comp426/jtizon001.github.io/getData/",
+        url: "192.241.142.13/getData/",
         dataType: "JSON",
         // data: {"base":"btc","target":"usd","amount":"100"},
         // data: {"d": dataObject},
