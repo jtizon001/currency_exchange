1b8a56b4624aa6ab31e3b3bde35ecd613ce6649b
qwang22 <33671464+qwang22@users.noreply.github.com>
2017-12-12 17:47:33 -0500

    cleaned up history section and added graph fx

--- static/main.js
+++ static/main.js
@@ -88,19 +88,15 @@ $(document).ready(function () {
     $( document.getElementById('historyButton') ).click(function(e) {
       // e.stopPropagation();
       e.preventDefault();
-      var historyArray = $('form').serializeArray();
+
+      var historyArray = $('#historyForm').serializeArray();
       console.log(historyArray);
 
       var historyJsonstring = {}
       jQuery.each(historyArray, function() {
         historyJsonstring[this.name] = this.value || '';
       });
-      console.log(historyJsonstring);
       var historyDataObject = JSON.stringify(historyJsonstring);
-      
-      var historyDataObject2 = "{" + historyDataObject.substring(35, historyDataObject.length);
-
-      console.log(historyDataObject2);
 
 
 
@@ -112,7 +108,7 @@ $(document).ready(function () {
         // url: "http://192.241.142.13:80/getData/",
         url: "http://localhost:5000/getHistory",
         contentType: "application/json",
-        data: historyDataObject2,
+        data: historyDataObject,
         error: function(response){
           console.log("error in ajax call");
         },
@@ -123,7 +119,12 @@ $(document).ready(function () {
         
         stringHistoryResponse = JSON.stringify(response);
         stringHistorySymbol = JSON.stringify(historyArray[1]);
+
+        console.log(stringHistorySymbol, stringHistoryResponse);
+
         stringHistorySymbol2 = stringHistorySymbol.substring(stringHistorySymbol.length-5, stringHistorySymbol.length-2).toUpperCase();
+
+        console.log(stringHistorySymbol2);
 
         document.getElementById('historyResult').innerHTML = "<BR>On this date in history, the conversion rate was " + stringHistoryResponse.substring(8, stringHistoryResponse.length-1);
         // + " " + stringHistorySymbol2;
@@ -150,12 +151,61 @@ $(document).ready(function () {
 
     document.getElementsByClassName('content-box-area')[0].innerHTML = '';
     
-    $( document.getElementsByClassName('content-box-area') ).append( "<div class = 'content-box'>Enter a base currency and up to 2 target currencies to compare:<br><form enctype='application/json'> <input type='text' placeholder ='Base' name='baseCurr' id ='baseCurr'> <input type='text' placeholder ='Target' name='targetCurr' id='targetCurr t1'> <input type='text' placeholder='Target (optional)' name='targetCurr' id='targetCurr t2'><select><option value='' disabled selected>Past...</option><option value='past30Days'>Past 30 Days</option><option value='past12Months'>Past 12 Months</option><option value='past5Years'>Past 5 Years</option></select> <input class='btn btn-primary btn-lg' type='submit' name='submit' id = 'graphButton'></form> </div>");
+    $( document.getElementsByClassName('content-box-area') ).append( "<div class = 'content-box'>Enter a base currency and up to 2 target currencies to compare:<br><form enctype='application/json' id='graphForm'> <input type='text' placeholder ='Base' name='baseCurr' id ='baseCurr'> <input type='text' placeholder ='Target' name='targetCurr' id='targetCurr t1'> <select id='timeDropdown'><option value='' disabled selected>Past...</option><option value='past30Days'>Past 30 Days</option><option value='past12Months'>Past 12 Months</option><option value='past5Years'>Past 5 Years</option></select> <input class='btn btn-primary btn-lg' type='submit' name='submit' id = 'graphButton'></form> </div>");
 
-    $( document.getElementsByClassName('content-box-area') ).append( "<div id = 'chart'></div>");
+    $("#graphButton").click(function(e) {
+      e.preventDefault();
+
+      var range = document.getElementById('timeDropdown').value
+      var array = $('#graphForm').serializeArray();
+      console.log(array);
+
+      var graphJson = {}
+      jQuery.each(array, function() {
+        graphJson[this.name] = this.value || '';
+      });
+      console.log(graphJson);
+      var graphJsonObject = JSON.stringify(graphJson);
+      console.log(graphJsonObject);
+
+      var graphJsonObj2 = graphJsonObject.substring(0, graphJsonObject.length-1) + ',"range":' + '"' + range + '"}';
+
+      console.log(graphJsonObj2);
+
+
+
+
+
+      $.ajax({
+      method: "POST",
+      url: "http://localhost:5000/getGraphData",
+      contentType: "application/json",
+      data: graphJsonObj2,
+      error: function(response) {
+        console.log("error graph");
+      },
+      success: function(response) {
+        console.log("success graph");
+
+        
+        $( document.getElementsByClassName('content-box-area') ).append( "<div id = 'chart'></div>");
+        makeGraph();
+      }
+
+    }).done(function() {
+
+      console.log("done");
+    })
+
+    })
+
+
+
+    //$( document.getElementsByClassName('content-box-area') ).append( "<div id = 'chart'></div>");
+    //makeGraph();
   });
+// _____________________________________content box: Maps____________________________________________
 
-// _________________________ content box: Maps _______________________________________________________
 
 
   $( document.getElementById('go-3') ).click(function() {
@@ -167,15 +217,14 @@ $(document).ready(function () {
 
   });
 
-// _________________________ content box: Travel News _______________________________________________________
+//__________________________________content box: Travel news_______________________________________________________
 
 
   $( document.getElementById('go-4') ).click(function() {
 
     document.getElementsByClassName('content-box-area')[0].innerHTML = '';
 
-    $( document.getElementsByClassName('content-box-area') ).append( "<div class = 'content-box'id = 'content-box-map'><iframe width='100%' height='400' style='border:none' src='http://output46.rssinclude.com/output?type=iframe&amp;id=1164374&amp;hash=92e11872b1c3456f40e011c43086ad7e'></iframe></div>");
-
+     $( document.getElementsByClassName('content-box-area') ).append( "<div class = 'content-box'id = 'content-box-map'>&nbsp;&nbsp;&nbsp;&nbsp;<iframe width='100%' height='400' style='border:none;'' src='http://output15.rssinclude.com/output?type=iframe&amp;id=1164374&amp;hash=92e11872b1c3456f40e011c43086ad7e'></iframe></div>");
   });
 
 
@@ -237,7 +286,9 @@ $(document).ready(function () {
   //chart
 // 2 arrays with dummy currency rate info of certain time frame
 // these 2 arrays should be returned by some database access call
-  //  $(function(){
+
+    /*
+
     $( document.getElementById('go-2') ).click(function() {
         var currency1 = [10, 20, 30, 40, 50]
         var currency2 = [5, 20, 40, 55, 60]
@@ -278,4 +329,47 @@ $(document).ready(function () {
         });
 
     });
+
+    */
+
+    var makeGraph = function() {
+      var currency1 = [10, 20, 30, 40, 50]
+      var currency2 = [5, 20, 40, 55, 60]
+
+//array of dates corresponding to above currency rates
+      var x = ['2016-01-01', '2016-01-02', '2016-01-03', '2016-01-04', '2016-01-05']
+
+// Needed for proper format of graph creation
+// Adds currency name to head of array
+      currency1.unshift('currency1');  //currency1 = ['currency1', 10, 20, 30,...]
+      currency2.unshift('currency2');  //currency2 = ['currency2', 5, 20, 40,...]
+
+      x.unshift('x');  //x = ['x', '2016-01-01',....]
+
+      var chart = c3.generate({
+
+        // 'bindto' to specify which html element to add graph to
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [x, currency1, currency2]
+        },
+        axis: {
+          x: {
+            type: 'timeseries'
+          }
+        }
+      });
+
+      // Not sure if necessary, just something extra
+      $("#chart").append($("<button id='show'>Show (currency2)</button>"));
+      $("#chart").append($("<button id='hide'>Hide (currency2)</button>"));
+      document.getElementById('show').addEventListener('click', function() {
+        chart.show(['currency2'])
+      });
+      document.getElementById('hide').addEventListener('click', function() {
+        chart.hide(['currency2'])
+      });
+
+    }
 });
